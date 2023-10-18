bd1b0837b6fdaa14ecf8f9600825b60f1e7665a9
kirill <golub.kirill12@gmail.com>
2023-10-18 12:41:33 +0300

    Тестовый проект

---
+++ Test1App/Test1App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test1App
+//
+//  Created by Kirill Ter on 18.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
